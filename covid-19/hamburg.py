bd1eb49d18f13ee573cedde7dd92010552ceedb8
Ömür Özkir <oemuer.oezkir@gmail.com>
2020-06-05 09:42:25 +0200

    small refactoring: reduces the interface of the hamburg module

--- covid-19/hamburg.py
+++ covid-19/hamburg.py
@@ -7,32 +7,45 @@ import requests
 from bs4 import BeautifulSoup
 
 
-def parse_infections(soup):
+def crawl_covid19_stats():
+    """Crawls hamburg.de for detailed covid-19 numbers and returns them as a dict"""
+    url = "https://www.hamburg.de/corona-zahlen/"
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+    infections = _parse_infections(soup)
+    deaths = _parse_deaths(soup)
+    hospitalizations = _parse_hospitalizations(soup)
+    boroughs = _parse_boroughs(soup)
+    trend = _parse_trend(soup)
+    return json.dumps({**infections, **deaths, **hospitalizations, **boroughs, **trend})
+
+
+def _parse_infections(soup):
     """Parses current infection numbers from the crawled webpage"""
-    return parse_body(
+    return _parse_body(
         soup,
         ["confirmed-total", "recovered", "new infections"],
         ".c_chart.one .chart_legend li",
     )
 
 
-def parse_deaths(soup):
+def _parse_deaths(soup):
     """Parses the current numbers for deaths due to covid-19 from the webpage"""
-    return parse_body(
+    return _parse_body(
         soup, ["deaths-total", "new deaths"], ".c_chart.two .chart_legend li"
     )
 
 
-def parse_hospitalizations(soup):
+def _parse_hospitalizations(soup):
     """Parses the current numbers for hospitalizations from the webpage"""
-    return parse_body(
+    return _parse_body(
         soup,
         ["hospitalizations-total", "intensive-care", "intensive-care-from-hamburg"],
         ".c_chart.three .chart_legend li",
     )
 
 
-def parse_body(soup, labels, selector):
+def _parse_body(soup, labels, selector):
     """Parse selected content from the crawled webpage and label it"""
     tags = soup.select(selector)
     tags = [re.search(r"\d+$", tag.text) for tag in tags]
@@ -40,7 +53,7 @@ def parse_body(soup, labels, selector):
     return dict(zip(labels, tags))
 
 
-def parse_boroughs(soup):
+def _parse_boroughs(soup):
     """Parse borough data from webcrawler"""
     tags = soup.select(".table-article tr")[18:]
     tags = [tag.find_all("td") for tag in tags]
@@ -55,29 +68,16 @@ def parse_boroughs(soup):
     return {"boroughs": tags}
 
 
-def parse_trend(soup):
+def _parse_trend(soup):
     """Parses the seven-day chart"""
     tags = soup.select(".cv_chart_container .value_show")
     tags = [int(tag.text) for tag in tags]
     return {"new_confirmed_in_last_seven_days": tags}
 
 
-def collect_stats():
-    """Crawls hamburg.de for detailed covid-19 numbers"""
-    url = "https://www.hamburg.de/corona-zahlen/"
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, "html.parser")
-    infections = parse_infections(soup)
-    deaths = parse_deaths(soup)
-    hospitalizations = parse_hospitalizations(soup)
-    boroughs = parse_boroughs(soup)
-    trend = parse_trend(soup)
-    return json.dumps({**infections, **deaths, **hospitalizations, **boroughs, **trend})
-
-
 def main():
     """The data is being updated on a daily basis"""
-    print(collect_stats())
+    print(crawl_covid19_stats())
 
 
 if __name__ == "__main__":
